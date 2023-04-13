d601433e5df5b10073abccf9741e9642bdad73a4
amber5678 <125154485+amber5678@users.noreply.github.com>
2023-04-13 14:51:05 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -17,10 +17,26 @@ public class GUI {
         JOptionPane.showMessageDialog(null, "Connection established successfully",
                 "Connection established successfully", JOptionPane.INFORMATION_MESSAGE);
     }
-    public void welcomeMenu() {
+    public void welcomeMenu() { //after the showWelcome GUI
         String[] options = {"Login", "Create an account", "Quit"};
         String welcomeOption = (String) JOptionPane.showInputDialog(null,
                 "Please select an option:", "Welcome Menu", JOptionPane.PLAIN_MESSAGE, null,
                 options, null);
+    }
+    public void loginMenu() { //if login is selected in the welcome menu
+        JOptionPane.showMessageDialog(null, "Login Menu",
+                "Login Menu", JOptionPane.INFORMATION_MESSAGE);
+    }
+    public void newAccountMenu() { //if create new account is selected in the welcome menu
+        JOptionPane.showMessageDialog(null, "Create New Account",
+                "Create New Account", JOptionPane.INFORMATION_MESSAGE);
+    }
+    public void quit() { //if quit is selected in the welcome menu
+        JOptionPane.showMessageDialog(null, "Bye!",
+                "Bye!", JOptionPane.INFORMATION_MESSAGE);
+    }
+    public void invalidWelcomeOption() { //if any other thing is selected in the welcome menu
+        JOptionPane.showMessageDialog(null, "Invalid Option!",
+                "Invalid Option!", JOptionPane.INFORMATION_MESSAGE);
     }
 }
