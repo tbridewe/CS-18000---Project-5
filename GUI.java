7c9a081662e11920b2b7d6e61aaaf78257647b7f
amber5678 <125154485+amber5678@users.noreply.github.com>
2023-05-01 15:54:42 -0400

    SellerNewEmail() show all buttons

--- GUI.java
+++ GUI.java
@@ -2405,7 +2405,7 @@ public class GUI {
         JPanel enterPanel = new JPanel();
         JPanel backPanel = new JPanel();
         Container content = frame.getContentPane();
-        frame.setSize(600, 300);
+        frame.setSize(1500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(emailPanel);
         content.add(backPanel);
